cf408459290b2be7c6dbd8038bb1f71e5e4ca3c3
thecookietho <thecookietho@gmail.com>
2025-12-16 11:56:20 +0100

    moved kd editor to own header file

--- elexon-incl/kd_editor.lua
+++ elexon-incl/kd_editor.lua
@@ -1,12 +1,11 @@
-return function(parent_menu)
-    if not parent_menu then
-        log.error("Parent menu is nil in kd_editor")
+return function(parent)
+    if not parent then
+        log.error("KD Editor parent menu is nil")
         return
     end
-
-    -- KD Editor root tab (child of Recovery)
-    local KDS = parent_menu:add_tab("KD Editor")
-
+    -- ===============================
+    -- Helper
+    -- ===============================
     local function show_kd_message(title, kd, kills, deaths, frozen)
         gui.show_message(
             title,
@@ -20,18 +19,20 @@ return function(parent_menu)
         )
     end
 
-    -- Custom KD
-    local TypeKd = KDS:add_tab("Custom KD")
-    TypeKd:add_text("Set your KD to:")
+    -- ======================================================
+    -- CUSTOM KD
+    -- ======================================================
+    parent:add_text("Custom K/D")
+    parent:add_separator()
 
-    local KdValue = TypeKd:add_input_float("Set your KD")
+    local custom_kd_input = parent:add_input_float("KD Value")
 
-    TypeKd:add_button("Set KD", function()
-        local kd = KdValue:get_value()
-        local base = 1000000
+    parent:add_button("Set Custom KD", function()
+        local kd = custom_kd_input:get_value()
+        local base = 100000
 
         local kills, deaths
-        if kd == 0 then
+        if kd <= 0 then
             kills = 0
             deaths = base
         else
@@ -42,38 +43,48 @@ return function(parent_menu)
         stats.set_int("MPPLY_KILLS_PLAYERS", kills)
         stats.set_int("MPPLY_DEATHS_PLAYER", deaths)
 
-        show_kd_message("KD Change", kd, kills, deaths, false)
+        show_kd_message("KD Changed", kd, kills, deaths, false)
     end)
 
-    -- Kills / Deaths Editor
-    local KillDeathEdit = KDS:add_tab("KILLS/DEATHS EDITOR")
-    local KillValue = KillDeathEdit:add_input_int("Kills")
-    local DeathValue = KillDeathEdit:add_input_int("Deaths")
+    parent:add_separator()
 
-    KillDeathEdit:add_button("Set Kills & Deaths", function()
-        local kills = KillValue:get_value()
-        local deaths = DeathValue:get_value()
-        if deaths <= 0 then deaths = 1 end
+    -- ======================================================
+    -- KILLS / DEATHS
+    -- ======================================================
+    parent:add_text("Kills / Deaths Editor")
+    parent:add_separator()
+
+    local kills_input  = parent:add_input_int("Kills")
+    local deaths_input = parent:add_input_int("Deaths")
+
+    parent:add_button("Set Kills & Deaths", function()
+        local kills  = kills_input:get_value()
+        local deaths = math.max(deaths_input:get_value(), 1)
 
         local kd = kills / deaths
 
         stats.set_int("MPPLY_KILLS_PLAYERS", kills)
         stats.set_int("MPPLY_DEATHS_PLAYER", deaths)
 
-        show_kd_message("KD Change", kd, kills, deaths, false)
+        show_kd_message("KD Changed", kd, kills, deaths, false)
     end)
 
-    -- Frozen KD
-    local FrozenKD = KDS:add_tab("Frozen K/D Editor")
-    FrozenKD:add_text("Set your frozen K/D to:")
-    local FrozenKdValue = FrozenKD:add_input_float("Frozen KD")
+    parent:add_separator()
 
-    FrozenKD:add_button("FROZEN CUSTOM K/D", function()
-        local kd = FrozenKdValue:get_value()
+    -- ======================================================
+    -- FROZEN KD
+    -- ======================================================
+    parent:add_text("Frozen K/D")
+    parent:add_separator()
+
+    local frozen_kd_input = parent:add_input_float("Frozen KD")
+
+    parent:add_button("Set Frozen KD", function()
+        local kd = frozen_kd_input:get_value()
         local base = 100000
 
         local kills, deaths
-        if kd == 0 then
+        if kd <= 0 then
             kills = 0
             deaths = base
         else
@@ -84,6 +95,6 @@ return function(parent_menu)
         stats.set_int("MPPLY_KILLS_PLAYERS", kills)
         stats.set_int("MPPLY_DEATHS_PLAYER", deaths)
 
-        show_kd_message("KD Change", kd, kills, deaths, true)
+        show_kd_message("Frozen KD Set", kd, kills, deaths, true)
     end)
 end
